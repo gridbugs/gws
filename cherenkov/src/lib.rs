b1d6f20ff1a4a744079a12f1a8875ed8b74625b8
Stephen Sherratt <sfsherratt@gmail.com>
2019-02-24 13:47:56 +0000

    Visible area detection, diminishing lighting

--- cherenkov/src/lib.rs
+++ cherenkov/src/lib.rs
@@ -47,12 +47,77 @@ impl InputGrid for Visibility {
     }
 }
 
+const VISION_DISTANCE: vision_distance::Circle = vision_distance::Circle::new(8);
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct VisibilityCell {
+    last_seen: u64,
+}
+
+#[derive(Clone, Copy)]
+pub struct VisibilityState {
+    count: u64,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct VisibileArea {
+    grid: Grid<VisibilityCell>,
+    count: u64,
+    #[serde(skip)]
+    shadowcast: ShadowcastContext<u8>,
+}
+
+impl VisibileArea {
+    pub fn new(size: Size) -> Self {
+        let grid = Grid::new_clone(size, VisibilityCell { last_seen: 0 });
+        let count = 1;
+        let shadowcast = ShadowcastContext::default();
+        Self {
+            grid,
+            count,
+            shadowcast,
+        }
+    }
+    pub fn state(&self) -> VisibilityState {
+        VisibilityState { count: self.count }
+    }
+    pub fn iter(&self) -> impl Iterator<Item = &VisibilityCell> {
+        self.grid.iter()
+    }
+    pub fn update(&mut self, player_coord: Coord, world: &Grid<Cell>) {
+        self.count += 1;
+        let count = self.count;
+        let grid = &mut self.grid;
+        self.shadowcast.for_each_visible(
+            player_coord,
+            &Visibility,
+            &world,
+            VISION_DISTANCE,
+            255,
+            |coord, direction_bitmap, visibility| {
+                grid.get_checked_mut(coord).last_seen = count;
+            },
+        );
+    }
+}
+
+impl VisibilityCell {
+    pub fn is_visible(&self, state: VisibilityState) -> bool {
+        self.last_seen == state.count
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct Cherenkov {
     grid: Grid<Cell>,
+    visible_area: VisibileArea,
     player_coord: Coord,
-    #[serde(skip)]
-    shadowcast: ShadowcastContext<u8>,
+}
+
+pub struct ToRender<'a> {
+    pub grid: &'a Grid<Cell>,
+    pub visible_area: &'a VisibileArea,
+    pub player_coord: &'a Coord,
 }
 
 impl Cherenkov {
@@ -76,11 +141,12 @@ impl Cherenkov {
                 _ => panic!(),
             }
         });
-        let shadowcast = ShadowcastContext::default();
+        let mut visible_area = VisibileArea::new(size);
+        visible_area.update(player_coord, &grid);
         Self {
             grid,
+            visible_area,
             player_coord,
-            shadowcast,
         }
     }
 
@@ -91,13 +157,14 @@ impl Cherenkov {
                 Input::Move(direction) => self.player_coord += direction.coord(),
             }
         }
+        self.visible_area.update(self.player_coord, &self.grid);
     }
 
-    pub fn player_coord(&self) -> Coord {
-        self.player_coord
-    }
-
-    pub fn grid(&self) -> &Grid<Cell> {
-        &self.grid
+    pub fn to_render(&self) -> ToRender {
+        ToRender {
+            grid: &self.grid,
+            visible_area: &self.visible_area,
+            player_coord: &self.player_coord,
+        }
     }
 }
--- prototty/src/game_view.rs
+++ prototty/src/game_view.rs
@@ -3,20 +3,70 @@ use prototty::*;
 
 pub struct GameView;
 
-const FLOOR: ViewCell = ViewCell::new().with_character('.');
+const FLOOR_BACKGROUND: Rgb24 = Rgb24::new(0, 0, 127);
+const FLOOR_FOREGROUND: Rgb24 = Rgb24::new(255, 255, 255);
+const WALL_TOP_COLOUR: Rgb24 = Rgb24::new(200, 128, 0);
+const WALL_FRONT_COLOUR: Rgb24 = Rgb24::new(200, 50, 0);
+
+const FLOOR: ViewCell = ViewCell::new()
+    .with_character('.')
+    .with_foreground(FLOOR_FOREGROUND)
+    .with_background(FLOOR_BACKGROUND);
 const WALL_ABOVE_FLOOR: ViewCell = ViewCell::new()
     .with_character('▀')
-    .with_background(Rgb24::new(127, 127, 127));
-const WALL_ABOVE_WALL: ViewCell = ViewCell::new().with_character('█');
+    .with_foreground(WALL_TOP_COLOUR)
+    .with_background(WALL_FRONT_COLOUR);
+const WALL_ABOVE_WALL: ViewCell = ViewCell::new()
+    .with_character('█')
+    .with_foreground(WALL_TOP_COLOUR)
+    .with_background(WALL_FRONT_COLOUR);
 const PLAYER: ViewCell = ViewCell::new().with_character('@');
+
+const LIGHT_DIMINISH_DAMPEN: u32 = 4;
+
+#[derive(Clone, Copy)]
+struct Rational {
+    num: u32,
+    denom: u32,
+}
+
+fn mult_channel(c: u8, by: Rational) -> u8 {
+    ((c as u32 * by.num) / by.denom) as u8
+}
+
+fn mult_rgb24(Rgb24 { red, green, blue }: Rgb24, by: Rational) -> Rgb24 {
+    Rgb24 {
+        red: mult_channel(red, by),
+        green: mult_channel(green, by),
+        blue: mult_channel(blue, by),
+    }
+}
+
+fn mult_cell_info(cell_info: &mut ViewCell, by: Rational) {
+    if let Some(foreground) = cell_info.foreground.as_mut() {
+        *foreground = mult_rgb24(*foreground, by);
+    }
+    if let Some(background) = cell_info.background.as_mut() {
+        *background = mult_rgb24(*background, by);
+    }
+}
 
 impl View<Cherenkov> for GameView {
     fn view<G: ViewGrid>(&mut self, game: &Cherenkov, offset: Coord, depth: i32, grid: &mut G) {
-        for (coord, &cell) in game.grid().enumerate() {
-            let cell_info = match cell {
+        let to_render = game.to_render();
+        let visibility_state = to_render.visible_area.state();
+        for ((coord, &cell), visibility) in to_render
+            .grid
+            .enumerate()
+            .zip(to_render.visible_area.iter())
+        {
+            if !visibility.is_visible(visibility_state) {
+                continue;
+            }
+            let mut cell_info = match cell {
                 Cell::Floor => FLOOR,
                 Cell::Wall => {
-                    if let Some(cell_below) = game.grid().get(coord + Coord::new(0, 1)) {
+                    if let Some(cell_below) = to_render.grid.get(coord + Coord::new(0, 1)) {
                         match cell_below {
                             Cell::Floor => WALL_ABOVE_FLOOR,
                             Cell::Wall => WALL_ABOVE_WALL,
@@ -26,8 +76,20 @@ impl View<Cherenkov> for GameView {
                     }
                 }
             };
+            let square_distance = {
+                let d = to_render.player_coord - coord;
+                d.x * d.x + d.y * d.y
+            } as u32;
+            let dampened_square_distance = (square_distance / LIGHT_DIMINISH_DAMPEN).max(1);
+            mult_cell_info(
+                &mut cell_info,
+                Rational {
+                    num: 1,
+                    denom: dampened_square_distance,
+                },
+            );
             grid.set_cell(offset + coord, depth, cell_info);
         }
-        grid.set_cell(offset + game.player_coord(), depth, PLAYER);
+        grid.set_cell(offset + to_render.player_coord, depth, PLAYER);
     }
 }
