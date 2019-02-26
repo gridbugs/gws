224201cbe45e9389bcf8efd61e054a814d4e7087
Stephen Sherratt <sfsherratt@gmail.com>
2019-02-26 07:41:09 +0000

    Experiment with wasm storage key name

--- wasm/js/index.js
+++ wasm/js/index.js
@@ -16,7 +16,8 @@ wasm.then(async wasm => {
         cell_width_px: 16,
         cell_height_px: 16,
     };
-    let storage_key = window.location.pathname + window.location.hash;
+    let _storage_key = window.location.pathname + window.location.hash;
+    let storage_key = "test";
     console.log("Using storage key: ", storage_key);
     let context = await new Context(config).with_storage(storage_key);
     let app = new wasm.WebApp(context.grid(), context.storage());
